9df6c53c7ea8ee6d1f29a421ef37a7d767a9c08d
ramu <ramu25251944@gmail.com>
2023-06-28 09:23:03 +0530

    Location

--- src/main/java/com/slokam/script/controller/LocationTypeController.java
+++ src/main/java/com/slokam/script/controller/LocationTypeController.java
@@ -1,18 +1,23 @@
 package com.slokam.script.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.slokam.script.dto.LocationTypeDto;
+import com.slokam.script.dto.ScriptDTO;
 import com.slokam.script.service.IlocationService;
 
 @RestController
-@RequestMapping("script")
+@RequestMapping("locationtype")
 public class LocationTypeController {
 	
 	@Autowired
@@ -28,5 +33,28 @@ public class LocationTypeController {
 	}
 	
 	
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<LocationTypeDto> findbylocationid(@PathVariable Integer id)
+	{
+		             LocationTypeDto loc =  ilocservice.getbyid(id);
+		       
+		       return ResponseEntity.status(HttpStatus.OK).body(loc);
+	}
+	
+	@GetMapping("deletebyid/{id}")
+	public ResponseEntity<LocationTypeDto> deletebyid(@PathVariable Integer id)
+	{
+		             LocationTypeDto locdtp = ilocservice.deletebyid(id);
+		    return ResponseEntity.status(HttpStatus.OK).body(locdtp);
+	}
+	
+	@GetMapping("getalllocation")
+	public ResponseEntity<List<LocationTypeDto>> getalllocationtype()
+	{
+		              List<LocationTypeDto> locationdto = ilocservice.getbyall();
+		      return ResponseEntity.status(HttpStatus.OK).body(locationdto);
+		
+	}
 
 }
--- src/main/java/com/slokam/script/service/IlocationService.java
+++ src/main/java/com/slokam/script/service/IlocationService.java
@@ -1,5 +1,7 @@
 package com.slokam.script.service;
 
+import java.util.List;
+
 import com.slokam.script.dto.LocationTypeDto;
 import com.slokam.script.entity.LocationType;
 
@@ -7,6 +9,9 @@ public interface IlocationService {
 	
 	
 	public LocationTypeDto save(LocationTypeDto loctypedto);
+	public LocationTypeDto getbyid(Integer id);
+	public List<LocationTypeDto> getbyall();
+	public LocationTypeDto deletebyid(Integer id);
 	
 
 }
